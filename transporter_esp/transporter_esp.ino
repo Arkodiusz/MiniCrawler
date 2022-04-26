8335e3ca98a46e289ff2518f2aeff97571d50e06
Arkodiusz <arecki358@gmail.com>
2022-04-26 12:40:59 +0200

    add communication led, hide wifi credentials, add git ignore

--- transporter_esp/transporter_esp.ino
+++ transporter_esp/transporter_esp.ino
@@ -1,11 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <Servo.h>
 #include <ESP8266WebServer.h>
+#include "WiFiCredentials.h"
 
-const char* ssid = "SIEC 6G";
-const char* password = "kmwtw999";
+const char* ssid = WIFI_SSID; // wifi credentials imported from WiFiCredentials.h (not staged to git) 
+const char* password = WIFI_PASSWORD;
 
-const uint8_t pinLed = 16;
+const uint8_t pinLedWiFi = 2;
+const uint8_t pinLedComm = 16;
 const uint8_t pinServoL = 5;
 const uint8_t pinServoR = 4;
 
@@ -18,7 +20,11 @@ const uint16_t L_STOP = 1455;
 const uint16_t R_STOP = 1435;
 const uint16_t MS_PER_SPEED_LEVEL = 20;
 
+unsigned long lastMillis;
+
 void controlMovement() {
+  digitalWrite(pinLedComm, LOW);
+  lastMillis = millis();
   String left = server.arg("left");
   String right = server.arg("right");
   int8_t speedLevelL = left.toInt();
@@ -50,21 +56,23 @@ void connectToWiFi() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    digitalWrite(pinLed, !digitalRead(pinLed));
+    digitalWrite(pinLedWiFi, !digitalRead(pinLedWiFi));
   }
   Serial.print("\nConnected to: ");
   Serial.print(WiFi.SSID());
   Serial.print("\nIP address: ");
   Serial.print(WiFi.localIP());
-  digitalWrite(pinLed, LOW);
+  digitalWrite(pinLedWiFi, LOW);
 }
 
 void setup() {
   Serial.begin(9600);
   Serial.print("\n\n===============================");
 
-  pinMode(pinLed, OUTPUT);
-  digitalWrite(pinLed, HIGH);
+  pinMode(pinLedWiFi, OUTPUT);
+  digitalWrite(pinLedWiFi, HIGH);
+  pinMode(pinLedComm, OUTPUT);
+  digitalWrite(pinLedComm, HIGH);
 
   servoL.attach(pinServoL);
   servoR.attach(pinServoR);
@@ -80,4 +88,7 @@ void setup() {
 
 void loop() {
   server.handleClient();
+  if (millis() > lastMillis + 100) {
+    digitalWrite(pinLedComm, HIGH);
+  }
 }
