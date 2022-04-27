f682173b6f62c40d4fd51c1910feb56d00f22b57
Arkodiusz <arecki358@gmail.com>
2022-04-28 00:22:24 +0200

    use motors instead of servos

--- transporter_esp/transporter_esp.ino
+++ transporter_esp/transporter_esp.ino
@@ -3,84 +3,117 @@
 #include <ESP8266WebServer.h>
 #include "WiFiCredentials.h"
 
-const char* ssid = WIFI_SSID; // wifi credentials imported from WiFiCredentials.h (not staged to git) 
-const char* password = WIFI_PASSWORD;
+const uint8_t LED_WIFI = 2;
+const uint8_t LED_COMM = 16;
 
-const uint8_t pinLedWiFi = 2;
-const uint8_t pinLedComm = 16;
-const uint8_t pinServoL = 5;
-const uint8_t pinServoR = 4;
+const uint8_t L_MOTOR_SPEED = 5;
+const uint8_t L_MOTOR_FWD = 4;
+const uint8_t L_MOTOR_BWD = 0;
+
+const uint8_t R_MOTOR_SPEED = 13;
+const uint8_t R_MOTOR_FWD = 14;
+const uint8_t R_MOTOR_BWD = 12;
+
+const uint8_t PWM_FREQUENCY = 1000;
+const uint8_t PWM_RANGE = 100;
+const uint8_t PWM_DUTY_CYCLE_PER_LEVEL = 10;
 
 ESP8266WebServer server(80);
 
-Servo servoL;
-Servo servoR;
-
-const uint16_t L_STOP = 1455;
-const uint16_t R_STOP = 1435;
-const uint16_t MS_PER_SPEED_LEVEL = 20;
-
 unsigned long lastMillis;
 
-void controlMovement() {
-  digitalWrite(pinLedComm, LOW);
+void handleMovementHttpRequest() {
+  digitalWrite(LED_COMM, LOW);
   lastMillis = millis();
   String left = server.arg("left");
   String right = server.arg("right");
   int8_t speedLevelL = left.toInt();
   int8_t speedLevelR = right.toInt();
-  servoController(speedLevelL, speedLevelR);
+  setMotorsRotation(speedLevelL, speedLevelR);
   server.send(204, "");
 }
 
-void servoController(int8_t speedLevelL, int8_t speedLevelR) {
-  int16_t speedL = speedLevelL * MS_PER_SPEED_LEVEL;
-  int16_t speedR = speedLevelR * MS_PER_SPEED_LEVEL;
+void setMotorsRotation(int8_t speedLevelL, int8_t speedLevelR) {  
+  if (speedLevelL > 0) {
+    digitalWrite(L_MOTOR_FWD, HIGH);
+    digitalWrite(L_MOTOR_BWD, LOW);
+  } else if (speedLevelL < 0) {
+    digitalWrite(L_MOTOR_FWD, LOW);
+    digitalWrite(L_MOTOR_BWD, HIGH);
+  } else {
+    digitalWrite(L_MOTOR_FWD, LOW);
+    digitalWrite(L_MOTOR_BWD, LOW);
+  }
+
+  if (speedLevelR > 0) {
+    digitalWrite(R_MOTOR_FWD, HIGH);
+    digitalWrite(R_MOTOR_BWD, LOW);
+  } else if (speedLevelR < 0) {
+    digitalWrite(R_MOTOR_FWD, LOW);
+    digitalWrite(R_MOTOR_BWD, HIGH);
+  } else {
+    digitalWrite(R_MOTOR_FWD, LOW);
+    digitalWrite(R_MOTOR_BWD, LOW);
+  }
+
+  int16_t speedL = abs(speedLevelL * PWM_DUTY_CYCLE_PER_LEVEL);
+  int16_t speedR = abs(speedLevelR * PWM_DUTY_CYCLE_PER_LEVEL);
 
   Serial.print("\n LEFT: ");
   Serial.print(speedL);  
-  Serial.print(" => ");
-  Serial.print(L_STOP + speedL);
   Serial.print("\nRIGHT: ");  
   Serial.print(speedR); 
-  Serial.print(" => ");  
-  Serial.print(R_STOP - speedR);
-  
-  servoL.writeMicroseconds(L_STOP + speedL);
-  servoR.writeMicroseconds(R_STOP - speedR);
+
+  analogWrite(L_MOTOR_SPEED, speedL);
+  analogWrite(R_MOTOR_SPEED, speedR);
 }
 
 void connectToWiFi() {
-  WiFi.begin(ssid, password);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); // wifi credentials imported from WiFiCredentials.h (not staged to git) 
   Serial.print("\n\nConnecting to WiFi.");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    digitalWrite(pinLedWiFi, !digitalRead(pinLedWiFi));
+    digitalWrite(LED_WIFI, !digitalRead(LED_WIFI));
   }
   Serial.print("\nConnected to: ");
   Serial.print(WiFi.SSID());
   Serial.print("\nIP address: ");
   Serial.print(WiFi.localIP());
-  digitalWrite(pinLedWiFi, LOW);
+  digitalWrite(LED_WIFI, LOW);
 }
 
 void setup() {
   Serial.begin(9600);
   Serial.print("\n\n===============================");
 
-  pinMode(pinLedWiFi, OUTPUT);
-  digitalWrite(pinLedWiFi, HIGH);
-  pinMode(pinLedComm, OUTPUT);
-  digitalWrite(pinLedComm, HIGH);
+  pinMode(LED_WIFI, OUTPUT);
+  digitalWrite(LED_WIFI, HIGH);
+  
+  pinMode(LED_COMM, OUTPUT);
+  digitalWrite(LED_COMM, HIGH);
 
-  servoL.attach(pinServoL);
-  servoR.attach(pinServoR);
-  servoController(0, 0);
+  pinMode(L_MOTOR_SPEED, OUTPUT);
+  pinMode(L_MOTOR_FWD, OUTPUT);
+  pinMode(L_MOTOR_BWD, OUTPUT);  
+  pinMode(R_MOTOR_SPEED, OUTPUT);
+  pinMode(R_MOTOR_FWD, OUTPUT);
+  pinMode(R_MOTOR_BWD, OUTPUT);
+
+  digitalWrite(L_MOTOR_FWD, LOW);
+  digitalWrite(L_MOTOR_BWD, LOW); 
+  digitalWrite(R_MOTOR_FWD, LOW);
+  digitalWrite(R_MOTOR_BWD, LOW);
+     
+  analogWriteRange(PWM_RANGE);
+  analogWriteFreq(PWM_FREQUENCY);
+  
+  analogWrite(L_MOTOR_SPEED, 0);
+  analogWrite(R_MOTOR_SPEED, 0);
 
   connectToWiFi();
   
-  server.on("/move", controlMovement);
+  server.on("/move", handleMovementHttpRequest);
   server.begin();
 
   delay(2000);
@@ -88,7 +121,7 @@ void setup() {
 
 void loop() {
   server.handleClient();
-  if (millis() > lastMillis + 100) {
-    digitalWrite(pinLedComm, HIGH);
+  if (!digitalRead(LED_COMM) && (millis() > lastMillis + 100)) {
+    digitalWrite(LED_COMM, HIGH);
   }
 }
